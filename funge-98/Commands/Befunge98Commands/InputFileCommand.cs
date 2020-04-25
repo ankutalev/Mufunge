7e3b268f563fa39620369d327d18eaf884106293
ankutalev <a.kutalev@g.nsu.ru>
2020-04-26 02:30:04 +0700

    i command added

--- funge-98/Commands/Befunge98Commands/InputFileCommand.cs
+++ funge-98/Commands/Befunge98Commands/InputFileCommand.cs
@@ -1,10 +1,12 @@
+using System;
+using System.IO;
+using System.Linq;
 using Attributes;
 using funge_98.ExecutionContexts;
 
 namespace funge_98.Commands.Befunge98Commands
 {
     [ContainerElement, Funge98]
-
     public class InputFileCommand : ICommand
     {
         public char Name { get; } = 'i';
@@ -12,8 +14,62 @@ namespace funge_98.Commands.Befunge98Commands
         public string RealExecute(FungeContext fungeContext)
         {
             //todo
+            var filename = fungeContext.PopString();
+            Console.WriteLine(filename);
+            var flag = fungeContext.GetTopStackTopValues(1)[0];
+            var vec = new DeltaVector(fungeContext.GetTopStackTopValues(fungeContext.Dimension).Reverse().ToArray());
+            if (!File.Exists(filename))
+            {
+                fungeContext.CurrentThreadDeltaVector = fungeContext.CurrentThreadDeltaVector.Reflect();
+                return null;
+            }
+
+            var y = 0;
+            int maxX = 0;
+            if ((flag & 1) == 1)
+            {
+                using var fs = File.OpenRead(filename);
+                using var reader = new BinaryReader(fs);
+                // Read in all pairs.
+                while (reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                    var target = vec + fungeContext.CurrentThread.StorageOffset;
+                    fungeContext.ModifyCell(target, reader.Read());
+                    target.X += 1;
+                }
+
+                maxX = (int) (reader.BaseStream.Length + fungeContext.CurrentThread.StorageOffset.X);
+            }
+            else
+            {
+                foreach (var line in File.ReadLines(filename))
+                {
+                    for (var x = 0; x < line.Length; x++)
+                    {
+                        if (line[x] == ' ' || line[x] == '\f')
+                            continue;
+                        fungeContext.ModifyCell(
+                            new DeltaVector(x, y, 0) + vec + fungeContext.CurrentThread.StorageOffset, line[x]);
+                    }
+
+                    if (line.Length > maxX)
+                    {
+                        maxX = line.Length;
+                    }
+
+                    y++;
+                }
+            }
+
+
+            fungeContext.PushToTopStack(maxX);
+            fungeContext.PushToTopStack(y);
+            foreach (var c in vec.Coords(fungeContext.Dimension))
+            {
+                fungeContext.PushToTopStack(c);
+            }
+
             return null;
-            // throw new System.NotImplementedException();
         }
     }
 }
